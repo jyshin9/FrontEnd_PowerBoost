7a09c42ba883f07c3de7a3090eb9f9d2f4bbc877
Juyoung Shin <126848983+jyshin9@users.noreply.github.com>
2024-07-29 19:40:04 +0900

    5주차 과제 업로드

---
+++ juyoung/week5/foodit/src/components/App.js
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from "react";
+import { createFood, updateFood, getFoods, deleteFood } from "../api";
+import FoodList from "./FoodList";
+import FoodForm from "./FoodForm";
+import { LocaleProvider } from "../contexts/LocaleContext";
+import LocaleSelect from "./LocaleSelect";
+
+function App() {
+  const [order, setOrder] = useState("createdAt");
+  const [cursor, setCursor] = useState(null);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+  const [search, setSearch] = useState("");
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
+
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setLoadingError(null);
+      setIsLoading(true);
+      result = await getFoods(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const {
+      foods,
+      paging: { nextCursor },
+    } = result;
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+      search,
+    });
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target["search"].value);
+  };
+
+  const handleCreateSuccess = (newItem) => {
+    setItems((prevItems) => [newItem, ...prevItems]);
+  };
+
+  const handleUpdateSuccess = (newItem) => {
+    setItems((prevItems) => {
+      const splitIdx = prevItems.findIndex((item) => item.id === newItem.id);
+      return [
+        ...prevItems.slice(0, splitIdx),
+        newItem,
+        ...prevItems.slice(splitIdx + 1),
+      ];
+    });
+  };
+
+  const handleDelete = async (id) => {
+    const result = await deleteFood(id);
+    if (!result) return;
+
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  useEffect(() => {
+    handleLoad({
+      order,
+      search,
+    });
+  }, [order, search]);
+
+  return (
+    <LocaleProvider>
+      <div>
+        <LocaleSelect />
+        <FoodForm onSubmit={createFood} onSubmitSuccess={handleCreateSuccess} />
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+        <form onSubmit={handleSearchSubmit}>
+          <input name="search" />
+          <button type="submit">검색</button>
+        </form>
+        <FoodList
+          items={sortedItems}
+          onUpdate={updateFood}
+          onUpdateSuccess={handleUpdateSuccess}
+          onDelete={handleDelete}
+        />
+        {cursor && (
+          <button disabled={isLoading} onClick={handleLoadMore}>
+            더보기
+          </button>
+        )}
+        {loadingError && <p>{loadingError.message}</p>}
+      </div>
+    </LocaleProvider>
+  );
+}
+
+export default App;
---
+++ juyoung/week5/foodit/src/components/FoodList.js
@@ -0,0 +1,84 @@
+import { useContext, useState } from 'react';
+import LocaleContext from '../contexts/LocaleContext';
+import FoodForm from './FoodForm';
+import './FoodList.css';
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function FoodListItem({ item, onEdit, onDelete }) {
+  const { imgUrl, title, calorie, content, createdAt } = item;
+  const locale = useContext(LocaleContext);
+
+  const handleEditClick = () => {
+    onEdit(item.id);
+  };
+
+  const handleDeleteClick = () => {
+    onDelete(item.id);
+  };
+
+  return (
+    <div className="FoodListItem">
+      <img src={imgUrl} alt={title} />
+      <div>{title}</div>
+      <div>{calorie}</div>
+      <div>{content}</div>
+      <div>{formatDate(createdAt)}</div>
+      <p>언어: {locale}</p>
+      <button onClick={handleEditClick}>수정</button>
+      <button onClick={handleDeleteClick}>삭제</button>
+    </div>
+  );
+}
+
+function FoodList({ items, onUpdate, onUpdateSuccess, onDelete }) {
+  const [editingId, setEditingId] = useState(null);
+
+  const handleCancel = () => {
+    setEditingId(null);
+  };
+
+  return (
+    <ul className="FoodList">
+      {items.map((item) => {
+        if (item.id === editingId) {
+          const { id, imgUrl, title, calorie, content } = item;
+          const initialValues = { title, calorie, content, imgFile: null };
+
+          const handleSubmit = (formData) => onUpdate(id, formData);
+
+          const handleSubmitSuccess = (newItem) => {
+            onUpdateSuccess(newItem);
+            setEditingId(null);
+          };
+
+          return (
+            <li key={item.id}>
+              <FoodForm
+                initialValues={initialValues}
+                initialPreview={imgUrl}
+                onSubmit={handleSubmit}
+                onSubmitSuccess={handleSubmitSuccess}
+                onCancel={handleCancel}
+              />
+            </li>
+          );
+        }
+        return (
+          <li key={item.id}>
+            <FoodListItem
+              item={item}
+              onEdit={setEditingId}
+              onDelete={onDelete}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default FoodList;
